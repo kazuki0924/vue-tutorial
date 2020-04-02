4e9e70b918c676c2f120248990d1283cb1c16101
kazuki0924 <42589054+kazuki0924@users.noreply.github.com>
2020-04-02 09:56:22 +0900

    4

--- components/src/main.js
+++ components/src/main.js
@@ -1,7 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import ServerStatus from './ServerStatus.vue';
+import ServerDetail from './ServerDetail.vue';
+import Servers from './Servers.vue';
+
+Vue.component('app-server-status', ServerStatus);
+Vue.component('app-server-detail', ServerDetail);
+Vue.component('app-servers', Servers);
 
 new Vue({
   el: '#app',
   render: h => h(App)
-})
+});
