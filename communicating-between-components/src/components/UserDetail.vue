6d5c5cc92721824415d6f0b1a9a0986b78ed2ffb
kazuki0924 <42589054+kazuki0924@users.noreply.github.com>
2020-04-04 13:05:32 +0900

    12

--- communicating-between-components/src/components/UserDetail.vue
+++ communicating-between-components/src/components/UserDetail.vue
@@ -3,6 +3,7 @@
     <h3>You may view the User Details here</h3>
     <p>Many Details</p>
     <p>User Name: {{ switchName() }}</p>
+    <button @click="resetName">Reset Name</button>
   </div>
 </template>
 
@@ -19,6 +20,9 @@ export default {
         .split('')
         .reverse()
         .join('');
+    },
+    resetName() {
+      this.myName = 'Max';
     }
   }
 };
