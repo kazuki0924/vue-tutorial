651326d98d29c7fde0d93b307575bab4e0783463
kazuki0924 <42589054+kazuki0924@users.noreply.github.com>
2020-04-07 00:29:27 +0900

    34

--- quotes-app/src/App.vue
+++ quotes-app/src/App.vue
@@ -1,14 +1,34 @@
 <template>
-  <div class="container"></div>
+  <div class="container">
+    <app-new-quote @quoteAdded="newQuote"></app-new-quote>
+    <app-quote-grid :quotes="quotes"></app-quote-grid>
+    <div class="row">
+      <div class="col-sm-12 text-center">
+        <alert class="alert-info">Info: Click on a Quote to delete it!</alert>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import QuoteGrid from './components/QuoteGrid.vue';
+import NewQuote from './components/NewQuote.vue';
+
 export default {
   data: function () {
     return {
       quotes: ['Just a Quote to see something'],
       maxQuotes: 10,
     };
+  },
+  methods: {
+    newQuote(quote) {
+      this.quotes.push(quote);
+    },
+  },
+  components: {
+    appQuoteGrid: QuoteGrid,
+    appNewQuote: NewQuote,
   },
 };
 </script>
