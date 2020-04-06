4c69a543e005125ec53301a538678e7e69b85805
kazuki0924 <42589054+kazuki0924@users.noreply.github.com>
2020-04-06 15:57:14 +0900

    30

--- dynamic-components/src/App.vue
+++ dynamic-components/src/App.vue
@@ -1,33 +1,40 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12">
-                <br>
-                <button class="btn btn-primary">Load Blue Template</button>
-                <button class="btn btn-success">Load Green Template</button>
-                <button class="btn btn-danger">Load Red Template</button>
-                <hr>
-                <app-blue></app-blue>
-                <app-green></app-green>
-                <app-red></app-red>
-            </div>
-        </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <br />
+        <button class="btn btn-primary" @click="selectedComponent = 'appBlue'">
+          Load Blue Template
+        </button>
+        <button class="btn btn-success" @click="selectedComponent = 'appGreen'">
+          Load Green Template
+        </button>
+        <button class="btn btn-danger" @click="selectedComponent = 'appRed'">
+          Load Red Template
+        </button>
+        <hr />
+        <p>{{ selectedComponent }}</p>
+        <component :is="selectedComponent"></component>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    import Blue from './components/Blue.vue';
-    import Green from './components/Green.vue';
-    import Red from './components/Red.vue';
+import Blue from './components/Blue.vue';
+import Green from './components/Green.vue';
+import Red from './components/Red.vue';
 
-    export default {
-        components: {
-            appBlue: Blue,
-            appGreen: Green,
-            appRed: Red
-        }
-    }
+export default {
+  data: function () {
+    return { selectedComponent: '' };
+  },
+  components: {
+    appBlue: Blue,
+    appGreen: Green,
+    appRed: Red,
+  },
+};
 </script>
 
-<style>
-</style>
+<style></style>
