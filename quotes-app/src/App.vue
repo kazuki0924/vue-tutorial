7c24a7118bf3b9f2da0f5cc6c315dbf4b6b911d5
kazuki0924 <42589054+kazuki0924@users.noreply.github.com>
2020-04-07 22:47:49 +0900

    36

--- quotes-app/src/App.vue
+++ quotes-app/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="container">
+    <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
     <app-new-quote @quoteAdded="newQuote"></app-new-quote>
     <app-quote-grid
       :quotes="quotes"
@@ -16,6 +17,7 @@
 <script>
 import QuoteGrid from './components/QuoteGrid.vue';
 import NewQuote from './components/NewQuote.vue';
+import Header from './components/Header.vue';
 
 export default {
   data: function () {
@@ -26,6 +28,9 @@ export default {
   },
   methods: {
     newQuote(quote) {
+      if (this.quotes.length >= this.maxQuotes) {
+        return alert('Please delete Quotes first!');
+      }
       this.quotes.push(quote);
     },
     deleteQuote(index) {
@@ -35,6 +40,7 @@ export default {
   components: {
     appQuoteGrid: QuoteGrid,
     appNewQuote: NewQuote,
+    appHeader: Header,
   },
 };
 </script>
