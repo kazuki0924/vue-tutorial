53a4159ec246abd2a97e5546e666d1c4d44eab4a
kazuki0924 <42589054+kazuki0924@users.noreply.github.com>
2020-04-08 17:59:06 +0900

    47

--- form/src/App.vue
+++ form/src/App.vue
@@ -5,12 +5,6 @@
         <div
           class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3"
         >
-          <!-- Exercise 1 -->
-          <!-- Create a Signup Form where you retrieve the following Information -->
-          <!-- Full Name (First Name + Last Name) -->
-          <!-- Mail -->
-          <!-- Password -->
-          <!-- Store Data? Yes/No -->
           <h1>Signup</h1>
           <hr />
           <div>
@@ -21,6 +15,10 @@
             <label for="last-name">Last Name</label>
             <input type="text" id="last-name" v-model="userData.lastName" />
           </div>
+          <app-full-name
+            :firstName="userData.firstName"
+            :lastName="userData.lastName"
+          ></app-full-name>
           <div>
             <label for="email">Mail</label>
             <input type="email" id="email" v-model="userData.email" />
@@ -43,13 +41,6 @@
               <button @click="submitted">Submit</button>
             </div>
           </div>
-          <!-- Exercise 2 -->
-          <!-- Only display the Form if it has NOT been submitted -->
-          <!-- Display the Data Summary ONCE the Form HAS been submitted -->
-
-          <!-- Exercise 3 -->
-          <!-- Edit the Example from above and create a custom "Full Name" Control -->
-          <!-- which still holds the First Name and Last Name Input Field -->
         </div>
       </div>
     </form>
@@ -61,7 +52,7 @@
             <h4>Your Data</h4>
           </div>
           <div class="panel-body">
-            <p>Full Name:</p>
+            <p>Full Name: {{ userData.firstName + ' ' + userData.lastName }}</p>
             <p>Mail: {{ userData.email }}</p>
             <p>Password: {{ userData.password }}</p>
             <p>Store in Database?: {{ storeData }}</p>
@@ -73,6 +64,7 @@
 </template>
 
 <script>
+import FullName from './FullName.vue';
 export default {
   data() {
     return {
@@ -90,6 +82,9 @@ export default {
     submitted() {
       this.isSubmitted = true;
     },
+  },
+  components: {
+    appFullName: FullName,
   },
 };
 </script>
---
+++ form/src/FullName.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>{{ firstName + ' ' + lastName }}</div>
+</template>
+
+<script>
+export default {
+  props: ['firstName', 'lastName'],
+};
+</script>
+
+<style></style>
