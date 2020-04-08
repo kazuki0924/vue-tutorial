5fc7f28a64643cf38bb35dd61aa64b4521374957
kazuki0924 <42589054+kazuki0924@users.noreply.github.com>
2020-04-08 15:58:57 +0900

    42

--- form/src/App.vue
+++ form/src/App.vue
@@ -101,6 +101,13 @@
           </select>
         </div>
       </div>
+      <div class="row">
+        <div
+          class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3"
+        >
+          <app-switch v-model="dataSwitch"></app-switch>
+        </div>
+      </div>
       <hr />
       <div class="row">
         <div
@@ -128,7 +135,7 @@
             </ul>
             <p>Gender: {{ gender }}</p>
             <p>Priority: {{ selectedPriority }}</p>
-            <p>Switched:</p>
+            <p>Switched: {{ dataSwitch }}</p>
           </div>
         </div>
       </div>
@@ -137,6 +144,8 @@
 </template>
 
 <script>
+import Switch from './Switch.vue';
+
 export default {
   data() {
     return {
@@ -150,7 +159,11 @@ export default {
       gender: 'Male',
       selectedPriority: 'High',
       priorities: ['High', 'Medium', 'Low'],
+      dataSwitch: true,
     };
+  },
+  components: {
+    appSwitch: Switch,
   },
 };
 </script>
