05f341218d2036454acd23d0cf3f4d442aa4ed96
kazuki0924 <42589054+kazuki0924@users.noreply.github.com>
2020-04-10 15:15:06 +0900

    55

--- custom-directives/src/App.vue
+++ custom-directives/src/App.vue
@@ -1,19 +1,38 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Directives Exercise</h1>
-                <!-- Exercise -->
-                <!-- Build a Custom Directive which works like v-on (Listen for Events) -->
-
-            </div>
-        </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Directives Exercise</h1>
+        <!-- Exercise -->
+        <!-- Build a Custom Directive which works like v-on (Listen for Events) -->
+        <button v-custom="clicked">Click Me</button>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {}
+export default {
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+  directives: {
+    custom: {
+      bind(el, binding) {
+        el.onClick = function () {
+          binding.value();
+        };
+      },
+    },
+  },
+  methods: {
+    clicked() {
+      alert('I was clicked!');
+    },
+  },
+};
 </script>
 
-<style>
-</style>
+<style></style>
