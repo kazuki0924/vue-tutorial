ef9fc318295bed2e784d25504f2fd97c25fbebf1
kazuki0924 <42589054+kazuki0924@users.noreply.github.com>
2020-04-10 09:41:51 +0900

    52

--- directives/src/App.vue
+++ directives/src/App.vue
@@ -11,13 +11,39 @@
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Custom Directives</h1>
         <p v-highlight:background.delayed="'red'">Color this</p>
+        <p v-local-highlight:background.delayed.blink="'red'">
+          Color this, too
+        </p>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  directives: {
+    'local-highlight': {
+      bind(el, binding, vnode) {
+        var delay = 0;
+        if (binding.modifiers['delayed']) {
+          delay = 3000;
+        }
+        if (binding.modifiers['blink']) {
+          let mainColor = 'green';
+          let secondColor = 'blue';
+        } else {
+          setTimeout(() => {
+            if (binding.arg == 'background') {
+              el.style.backgroundColor = binding.value;
+            } else {
+              el.style.color = binding.value;
+            }
+          }, delay);
+        }
+      },
+    },
+  },
+};
 </script>
 
 <style></style>
