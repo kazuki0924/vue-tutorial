812664ec0ba381dbe28eca916b2c08c391ff022e
kazuki0924 <42589054+kazuki0924@users.noreply.github.com>
2020-04-12 19:06:29 +0900

    65

--- transition/src/App.vue
+++ transition/src/App.vue
@@ -4,7 +4,7 @@
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Animations</h1>
         <hr />
-        <select v-model="alertAnimation">
+        <select v-model="alertAnimation" class="form-control">
           <option value="fade">Fade</option>
           <option value="slide">Slide</option>
         </select>
@@ -25,6 +25,35 @@
         >
           <div class="alert alert-info" v-if="show">This is some Info</div>
         </transition>
+        <transition :name="alertAnimation" mode="out-in">
+          <div class="alert alert-info" v-if="show" key="info">
+            This is some Info
+          </div>
+          <div class="alert alert-warning" v-else key="warning">
+            This is some Warning
+          </div>
+        </transition>
+        <hr />
+        <button class="btn btn-primary" @click="load = !load">
+          Load / Remove Element
+        </button>
+        <br /><br />
+        <transition
+          @before-enter="beforeEnter"
+          @enter="enter"
+          @after-enter="afterEnter"
+          @enter-cancelled="enterCancelled"
+          @before-leave="beforeLeave"
+          @leave="leave"
+          @after-leave="afterLeave"
+          @leave-cancelled="leaveCancelled"
+          :css="false"
+        >
+          <div
+            style="width: 300px; height: 100px; background-color: lightgreen;"
+            v-if="load"
+          ></div>
+        </transition>
       </div>
     </div>
   </div>
@@ -34,9 +63,60 @@
   export default {
     data() {
       return {
-        show: true,
+        show: false,
+        load: true,
         alertAnimation: 'fade',
+        elementWidth: 100,
       };
+    },
+    methods: {
+      beforeEnter(el) {
+        console.log('beforeEnter');
+        this.elementWidth = 100;
+        el.style.width = this.elementWidth + 'px';
+      },
+      enter(el, done) {
+        console.log('enter');
+        let round = 1;
+        const interval = setInterval(() => {
+          el.style.width = this.elementWidth + round * 10 + 'px';
+          round++;
+          if (round > 20) {
+            clearInterval(interval);
+            done();
+          }
+        }, 20);
+        done();
+      },
+      afterEnter(el) {
+        console.log('afterEnter');
+      },
+      enterCancelled(el) {
+        console.log('enterCancelled');
+      },
+      beforeLeave(el) {
+        console.log('beforeLeave');
+        this.elementWidth = 300;
+        this.style.width = this.elementWidth + 'px';
+      },
+      leave(el, done) {
+        console.log('leave');
+        let round = 1;
+        const interval = setInterval(() => {
+          el.style.width = this.elementWidth - round * 10 + 'px';
+          round++;
+          if (round > 20) {
+            clearInterval(interval);
+            done();
+          }
+        }, 20);
+      },
+      afterLeave(el) {
+        console.log('afterLeave');
+      },
+      leaveCancelled(el) {
+        console.log('leaveCancelled');
+      },
     },
   };
 </script>
