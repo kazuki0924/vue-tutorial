f99448c577b7624c2baa7be35eea9dac7e36e750
kazuki0924 <42589054+kazuki0924@users.noreply.github.com>
2020-04-16 17:38:12 +0900

    76

--- routing/src/components/user/User.vue
+++ routing/src/components/user/User.vue
@@ -1,13 +1,28 @@
 <template>
   <div>
     <h1>The User Page</h1>
+    <hr />
+    <p>Loaded ID: {{ id }}</p>
+    <button @click="navigateToHome" class="btn btn-primary">Go to Home</button>
   </div>
 </template>
 
 <script>
   export default {
+    data() {
+      return {
+        id: this.$route.params.id,
+      };
+    },
+    watch: {
+      $route(to, from) {
+        this.id = to.params.id;
+      },
+    },
     methods: {
-      navigateToHome() {},
+      navigateToHome() {
+        this.$router.push('/');
+      },
     },
   };
 </script>
