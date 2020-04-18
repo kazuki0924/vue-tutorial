0f89f05c4ea72d12fe8e01254750b1a50d784481
kazuki0924 <42589054+kazuki0924@users.noreply.github.com>
2020-04-18 13:22:09 +0900

    97

--- vuex/src/components/AnotherResult.vue
+++ vuex/src/components/AnotherResult.vue
@@ -7,9 +7,13 @@
 
 <script>
   import {mapGetters} from 'vuex';
+  import * as types from '../stores/types';
   export default {
     computed: {
-      ...mapGetters(['doubleCounter', 'stringCounter']),
+      ...mapGetters({
+        doubleCounter: types.DOUBLE_COUNTER,
+        stringCounter: types.CLICK_COUNTER,
+      }),
     },
   };
 </script>
